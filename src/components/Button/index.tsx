4102263dabed894f7560c5af7e878ffd686b079d
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-16 22:03:17 +0300

    feat: add customize to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import cn from 'classnames';
 import s from './Button.module.scss';
 
-interface ButtonProps {
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+export enum ButtonColors {
+  yellow = 'yellow',
+  green = 'green',
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
-    return (
-        <button type="button" className={s.root} onClick={onClick}>
-            { children }
-        </button>
-    );
+export enum ButtonSize {
+  default = 'default',
+  small = 'small',
+}
+
+interface ButtonProps {
+  noMaxWidth?: boolean;
+  color?: ButtonColors;
+  size?: ButtonSize;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  size = ButtonSize.default,
+  onClick,
+  color = ButtonColors.green,
+  noMaxWidth,
+}) => {
+  const selectBgColors = (propColor: string) => {
+    if (propColor === ButtonColors.green) {
+      return s.bgGreen;
+    }
+    if (propColor === ButtonColors.yellow) {
+      return s.bgYellow;
+    }
+  };
+
+  const selectSize = (propSize: string) => {
+    if (propSize === ButtonSize.default) {
+      return s.defaultSize;
+    }
+    if (propSize === ButtonSize.small) {
+      return s.smallSize
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={cn(s.root, selectBgColors(color), selectSize(size), { [s.noMaxWidth]: noMaxWidth })}
+      onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
