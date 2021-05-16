1f8aa8ee73634a8a6bd072668429f833dc42787a
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-16 18:03:20 +0300

    feat: add basic HomePage layout

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Home.module.scss';
+import Header from "../../components/Header";
+
+const HomePage = () => {
+    return (
+        <div className={s.root}>
+            <Header />
+        </div>
+    );
+};
+
+export default HomePage;
