c5ab66b829e680672fc20881ad71c1b979db28da
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-17 15:55:57 +0300

    fix: change HeadingTypes enum to object
    Cuz i dont understand why its not working with enum

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-export enum HeadingTypes {
-    h1 = "h1",
-    h2 = "h2",
-    h3 = "h3",
-    h4 = "h4",
-    h5 = "h5",
-    h6 = "h6",
-    Paragraph = "p",
-}
+
+export const HeadingTypes = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  Paragraph: 'p',
+};
 
 interface HeadingProps {
-    type: string;
+  type: string;
 }
 
-const Heading: React.FC<HeadingProps> = ({ children, type }) => {
-
-    return (
-        React.createElement(type, null, children)
-    );
+const Heading: React.FC<HeadingProps> = ({ type, children }) => {
+  return React.createElement(type, null, children);
 };
 
 export default Heading;
