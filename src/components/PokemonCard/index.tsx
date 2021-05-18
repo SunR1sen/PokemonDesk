16b413efcb44833c50c4e1fa348c704415cb61ca
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-19 02:52:40 +0300

    refactor: render PokemonCard in deps on props

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,38 +1,48 @@
 import React from 'react';
-import Heading, {HeadingTypes} from "../Heading";
+import cn from "classnames";
+import Heading, { HeadingTypes } from '../Heading';
 
 import s from './PokemonCard.module.scss';
 
-const PokemonCard = () => {
-    return (
-        <div className={s.root}>
-            <div className={s.infoWrap}>
-                <Heading type={HeadingTypes.Paragraph} className={s.titleName}>
-                    Charmander
-                </Heading>
-                <div className={s.statWrap}>
-                    <div className={s.statItem}>
-                        <div className={s.statValue}>
-                            52
-                        </div>
-                        Attack
-                    </div>
-                    <div className={s.statItem}>
-                        <div className={s.statValue}>
-                            43
-                        </div>
-                        Defense
-                    </div>
-                </div>
-                <div className={s.labelWrap}>
-                    <span className={s.label}>Fire</span>
-                </div>
-            </div>
-            <div className={s.pictureWrap}>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png" alt="Charmander" />
-            </div>
+interface PokemonProp {
+  name: string;
+  attack: number;
+  defense: number;
+  types: Array<string>;
+  imageSrc: string;
+}
+
+const PokemonCard: React.FC<PokemonProp> = ({ attack, defense, name, types, imageSrc }) => {
+  return (
+    <div className={s.root}>
+      <div className={s.infoWrap}>
+        <Heading type={HeadingTypes.Paragraph} className={s.titleName}>
+          {name}
+        </Heading>
+        <div className={s.statWrap}>
+          <div className={s.statItem}>
+            <div className={s.statValue}>{attack}</div>
+            Attack
+          </div>
+          <div className={s.statItem}>
+            <div className={s.statValue}>{defense}</div>
+            Defense
+          </div>
         </div>
-    );
+        <div className={s.labelWrap}>
+          {types.map((type) => (
+            <span className={cn(s.label, s[type as keyof typeof s])}>{type}</span>
+          ))}
+        </div>
+      </div>
+      <div className={s.pictureWrap}>
+        <img
+          src={imageSrc}
+          alt={name}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PokemonCard;
