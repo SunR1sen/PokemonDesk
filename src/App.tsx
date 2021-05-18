5e2af918d47a2ce50e9d9f586e483fcf2d454d14
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-18 18:02:08 +0300

    feat: use NotFound page when no match on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import { useRoutes } from 'hookrouter';
 import routes from './routes';
+import NotFoundPage from './pages/NotFound';
 
 const App = () => {
-  return useRoutes(routes);
+  const match = useRoutes(routes);
+
+  return match || <NotFoundPage />;
 };
 
 export default App;
