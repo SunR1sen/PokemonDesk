850211bc2d8dd1642178925bc0d96a1c3a7d2746
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-19 01:08:30 +0300

    refactor: change HeadingTypes from object to enum

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-
-export const HeadingTypes = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  h5: 'h5',
-  h6: 'h6',
-  Paragraph: 'p',
-};
+export enum HeadingTypes {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6',
+  Paragraph = 'p',
+}
 
 interface HeadingProps {
   type: string;
