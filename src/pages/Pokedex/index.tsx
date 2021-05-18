6de6188f261b5b5ff6745080491943efe0aabd0b
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-18 22:20:14 +0300

    feat: create Pokedex page

---
+++ src/pages/Pokedex/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+import PokemonCard from '../../components/PokemonCard';
+
+const Pokedex = () => {
+  return (
+    <>
+      <Header />
+      <PokemonCard />
+    </>
+  );
+};
+
+export default Pokedex;
