9d32f8374f88061c9c99ba704bf98a37f9eb9df2
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-18 17:29:03 +0300

    refactor : use GENERAL_MENU

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,38 +2,12 @@ import React from 'react';
 import { A, usePath } from 'hookrouter';
 import cn from 'classnames';
 import s from './Header.module.scss';
-import {ReactComponent as PokemonLogoSvg} from './assets/pokemonLogo.svg';
-
-interface IMenu {
-  id: number;
-  value: string;
-  link: string;
-}
-
-const MENU: IMenu[] = [
-  {
-    id: 1,
-    value: 'Home',
-    link: '#',
-  },
-  {
-    id: 2,
-    value: 'Pokédex',
-    link: '#',
-  },
-  {
-    id: 3,
-    value: 'Legendaries',
-    link: '#',
-  },
-  {
-    id: 4,
-    value: 'Documentation',
-    link: '#',
-  },
-];
+import { ReactComponent as PokemonLogoSvg } from './assets/pokemonLogo.svg';
+import { GENERAL_MENU } from '../../routes';
 
 const Header = () => {
+  const path = usePath();
+
   return (
     <div className={s.root}>
       <div className={s.wrap}>
@@ -41,7 +15,7 @@ const Header = () => {
           <PokemonLogoSvg />
         </div>
         <div className={s.menuWrap}>
-          {MENU.map(({ id, value, link }) => {
+          {GENERAL_MENU.map(({ title, link }) => {
             return (
               <A key={title} href={link} className={cn(s.menuLink, { [s.activeLink]: link === path })}>
                 {title}
