0273d9c56009df6b1d28a767a9e08a6152e0afb2
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-19 05:59:50 +0300

    feat: use Footer on Pokedex

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -5,6 +5,7 @@ import PokemonCard from '../../components/PokemonCard';
 import s from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
 import pokemonsMock from './pokemonsMock';
+import Footer from '../../components/Footer';
 
 const Pokedex = () => {
   return (
@@ -21,6 +22,7 @@ const Pokedex = () => {
           />
         ))}
       </Layout>
+      <Footer />
     </>
   );
 };
