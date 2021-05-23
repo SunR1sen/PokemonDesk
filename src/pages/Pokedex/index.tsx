24e1f554593a75701307a1450f795bb17d245934
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-23 15:31:26 +0300

    feat: add title to Pokedex

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from '../../components/PokemonCard';
 
 import s from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
 import pokemonsMock from './pokemonsMock';
 import Footer from '../../components/Footer';
+import Heading, { HeadingTypes } from '../../components/Heading';
 
 const Pokedex: React.FC = () => {
+  const [totalPokemons, setTotalPokemons] = useState(0);
+
   return (
-    <>
+    <div className={s.root}>
+      <Heading className={s.title} type={HeadingTypes.h2}>
+        {totalPokemons} <b>Pokemons</b> to you for choosing your favorite!
+      </Heading>
       <Layout className={s.cardsWrap}>
         {pokemonsMock.map((poke) => (
           <PokemonCard
