f838590c959a7cad576ec3f2cec0af363a757c54
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-23 19:37:31 +0300

    feat: create usePokemons to fetch data

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,30 +1,66 @@
-import React, { useState } from 'react';
-import PokemonCard from '../../components/PokemonCard';
+import React, { useEffect, useState } from 'react';
+import PokemonCard, {IPokemon} from '../../components/PokemonCard';
 
 import s from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
-import pokemonsMock from './pokemonsMock';
 import Footer from '../../components/Footer';
 import Heading, { HeadingTypes } from '../../components/Heading';
 
+type DataType = {
+  total: number;
+  limit: number;
+  offset: number;
+  pokemons: Array<IPokemon>
+}
+
+const usePokemons = () => {
+  const [data, setData] = useState<DataType>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      try {
+        const response = await fetch('https://zar.hosthot.ru/api/v1/pokemons');
+        const result = await response.json();
+
+        setData(result);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getPokemons().then();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
+
 const Pokedex: React.FC = () => {
-  const [totalPokemons, setTotalPokemons] = useState(0);
+  const { data, isLoading, isError } = usePokemons();
+
+  if (isLoading) {
+    return <div>Loading......</div>
+  }
+
+  if (isError) {
+    return <div>!!!SOMETHING GONE WRONG!!!</div>
+  }
 
   return (
     <div className={s.root}>
       <Heading className={s.title} type={HeadingTypes.h2}>
-        {totalPokemons} <b>Pokemons</b> to you for choosing your favorite!
+        {data?.total} <b>Pokemons</b> to you for choosing your favorite!
       </Heading>
       <Layout className={s.cardsWrap}>
-        {pokemonsMock.map((poke) => (
-          <PokemonCard
-            name={poke.name}
-            attack={Number(poke.stats.attack)}
-            defense={poke.stats.defense}
-            types={poke.types}
-            imageSrc={poke.img}
-            key={poke.id}
-          />
+        {data?.pokemons.map((poke: IPokemon) => (
+          <PokemonCard name={poke.name} stats={poke.stats} types={poke.types} img={poke.img} key={poke.id} />
         ))}
       </Layout>
       <Footer className={s.footer} />
