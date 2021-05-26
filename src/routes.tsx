0c37c019f9ccd34d7108d79232b22869b3ae8f90
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-27 01:57:48 +0300

    fix: delete useless name prop

--- src/routes.tsx
+++ src/routes.tsx
@@ -44,7 +44,7 @@ export const GENERAL_MENU: IGeneralMenuItem[] = [
 const SECOND_ROUTES: IGeneralMenuItem[] = [{
   title: 'Pokemon',
   link: LinkEnum.POKEMON,
-  component: ({id, name }: PokemonProps) => <Pokemon id={id} name={name} />
+  component: ({id }: PokemonProps) => <Pokemon id={id} />
 }]
 
 interface IAccMenu {
