a74ad1b4899a186bf786aaa3b161dac9ba072321
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-27 01:59:39 +0300

    feat: add optionally params field to url

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,9 +1,14 @@
 import config, { Endpoints } from '../config';
 
-function getUrlWithParamsConfig(endPointConfig: Endpoints, query: object) {
+function getUrlWithParamsConfig(endPointConfig: Endpoints, query: object, params?: any) {
+  let { pathname } = config.client.endpoint[endPointConfig as keyof typeof config.client.endpoint].uri;
+  if (params?.id) {
+    pathname += params.id;
+  }
+
   const url = {
     ...config.client.server,
-    ...config.client.endpoint[endPointConfig as keyof typeof config.client.endpoint].uri,
+    pathname,
     query: { ...query },
   };
 
