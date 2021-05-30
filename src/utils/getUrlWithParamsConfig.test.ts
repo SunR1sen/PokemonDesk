ded180d0f87ead0e7bfceeedd0a0291fa2955a34
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-30 22:10:36 +0300

    test: add test files with 3 basic test

---
+++ src/utils/getUrlWithParamsConfig.test.ts
@@ -0,0 +1,37 @@
+import getUrlWithParamsConfig from './getUrlWithParamsConfig';
+import { Endpoints } from '../config';
+
+describe('getUrlWithParamsConfig', () => {
+  test("Должна принимать 2 аргумента 'getPokemon' и пустой объект, на выходе получить объект с полями pathname, protocol, host и пустым query", () => {
+    const url = getUrlWithParamsConfig(Endpoints.GetPokemons, {});
+
+    expect(url).toEqual({
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemons',
+        query: {}
+    })
+  });
+
+    test("Должна принимать 2 аргумента 'getPokemon' и пустой объект, на выходе получить объект с полями pathname, protocol, host и query c полем name равным Pikachu", () => {
+        const url = getUrlWithParamsConfig(Endpoints.GetPokemons, { name: 'Pikachu'});
+
+        expect(url).toEqual({
+            protocol: 'http',
+            host: 'zar.hosthot.ru',
+            pathname: '/api/v1/pokemons',
+            query: { name: "Pikachu" }
+        })
+    });
+
+    test("Должна принимать 2 аргумента 'getPokemon' и { id: 25 }, на выходе получить объект с полями pathname, protocol, host и пустым query", () => {
+      const url = getUrlWithParamsConfig(Endpoints.GetPokemon, { id: 25 });
+
+      expect(url).toEqual({
+        protocol: 'http',
+        host: 'zar.hosthot.ru',
+        pathname: '/api/v1/pokemon/25',
+        query: {},
+      });
+    });
+});
