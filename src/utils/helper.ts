e7c5eae957dfb5c7ec6fe11114e1d6c108a0125b
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-05-31 03:02:45 +0300

    feat: add function to capitalize first letter in string

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,7 @@
 export function numberWithSpaces(x: number | string) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
+
+export function capitalizeFirstLetter(str: string) {
+    return str[0].toUpperCase() + str.substr(1);
+}
