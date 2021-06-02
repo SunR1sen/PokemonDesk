9da7abf546e0936dcd1ebfc1f52616924cd17a04
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-06-02 20:56:38 +0300

    feat: add setPath method to correctly work hookrouter

--- src/server/server.js
+++ src/server/server.js
@@ -1,31 +1,32 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
+import Hapi from '@hapi/hapi';
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import { setPath } from 'hookrouter';
+import App from '../App';
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/{any*}',
+    handler: (request, h) => {
+      setPath(request.path);
+      const result = ReactDOM.renderToString(<App />);
+      return result;
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/{any*}',
-        handler: (request, h) => {
-
-            return `Hello World! This is page ${request.path}`;
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
