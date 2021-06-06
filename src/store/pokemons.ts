99f34fa5249a92407edab3bdbc613caa727ef37f
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2021-06-06 21:51:58 +0300

    feat: add pokemon types reducer

---
+++ src/store/pokemons.ts
@@ -0,0 +1,46 @@
+const initialState = {
+    types: {
+        isLoading: false,
+        data: null,
+        error: null,
+    }
+}
+
+const pokemons = (state = initialState, action: { type: string; payload: any }) => {
+  switch (action.type) {
+    case 'FETCH_TYPES':
+      return {
+        ...state,
+        types: {
+          isLoading: true,
+          data: null,
+          error: null,
+        },
+      };
+
+    case 'FETCH_TYPES_RESOLVE':
+      return {
+        ...state,
+        types: {
+          isLoading: false,
+          data: action.payload,
+          error: null,
+        },
+      };
+
+    case 'FETCH_TYPES_REJECT':
+      return {
+        ...state,
+        types: {
+          isLoading: false,
+          data: null,
+          error: action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default pokemons;
